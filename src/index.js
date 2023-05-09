a6b7f67e918255ffdf9c9f9719bfc1f21312f652
Kahfi Kurnia Aji <58323098+kahfikurniaaji@users.noreply.github.com>
2023-05-10 01:32:20 +0700

    Initialization Project

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(process.env.PORT, process.env.HOST, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
